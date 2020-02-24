610a8b6f59e25c589a0545d694c3574957b532d5
Michael Wunderlich <mwunderl@amazon.com>
2020-02-23 16:38:03 -0800

    gson serializer

--- sample-apps/blank/function-java/src/main/java/example/handler.java
+++ sample-apps/blank/function-java/src/main/java/example/handler.java
@@ -1,23 +1,36 @@
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.StringBuilder;
+import java.util.Map;
 
 public class Handler implements RequestHandler<SQSEvent, String>{
-  // example.Handler::handleRequest
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+  // example.Handler::handleRequest
   @Override
   public String handleRequest(SQSEvent event, Context context)
   {
     LambdaLogger logger = context.getLogger();
-    logger.log(event.toString());
-    logger.log(context.toString());
+    logger.log("EVENT: " + gson.toJson(event));
+    logger.log("ENVIRONMENT VARIABLES: ");
+
+    Map<String, String> env = System.getenv();
+    StringBuilder envs = new StringBuilder();
+    env.forEach((key, value) -> envs.append(key + ": " + value + "\n"));
+    logger.log(envs.toString());
+
     for(SQSMessage msg : event.getRecords()){
       logger.log(new String(msg.getBody()));
     }
-    return "HELLO";
+    return context.getAwsRequestId();
   }
 }
