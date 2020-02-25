1315e00aa2ec4841d68ac1dd5c81cd16161c7a91
Michael Wunderlich <mwunderl@amazon.com>
2020-02-25 00:17:40 -0800

    cleanup

--- sample-apps/blank-java/invoke.sh
+++
@@ -1,1 +0,0 @@
-aws lambda invoke --function-name blank-java --payload file://event.json out
--- sample-apps/blank/function-java/update-code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-gradle build
-aws lambda update-function-code --function-name blank-java --zip-file fileb://build/distributions/function-java.zip
